8cfd707597089eac3ba023aea54eb17b2f244be0
Victor Lellis <vmlellis@gmail.com>
2017-11-12 19:07:25 -0200

    Document Service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component'
 import { ProposalNewComponent } from './proposal/proposal-new.component'
 import { ProposalShowComponent } from './proposal/proposal-show.component'
@@ -17,7 +19,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    HttpModule
   ],
   declarations: [
     AppComponent,
@@ -26,6 +29,9 @@ import { AppRoutingModule } from './app-routing.module';
     ProposalListComponent,
     ProposalNewComponent,
     ProposalShowComponent
+  ],
+  providers: [
+    DocumentService
   ],
   bootstrap: [
     AppComponent
